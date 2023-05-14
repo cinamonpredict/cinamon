6b8fb5065b042fad65300856bdd95e1e9222625d
cinamonpredict <133508254+cinamonpredict@users.noreply.github.com>
2023-05-14 13:17:42 +0530

    Update Grade.py

--- pages/Grade.py
+++ pages/Grade.py
@@ -5,15 +5,19 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 
-model = tf.keras.models.load_model("saved_model/gradee.hdf5")
+model = tf.keras.models.load_model("saved_model/mdgrade.hdf5")
 ### load file
 uploaded_file = st.file_uploader("Choose a image file")
 
-map_dict = { 0:'turmericfingers',
-             1:'turmericbulbs',
-             2:'sproutedturmeric',
-             3:'insectdamages',
-             4:'healthyrawturmeric',
+map_dict = { 0:'alba',
+1:'blackpeper_low',
+2:'c4',
+3:'c4_c5',
+4:'extraspecial',
+5:'faq',
+6:'grade_1',
+7:'grade_2',
+8:'NoGrade'
             }
 
 
@@ -37,3 +41,22 @@ if uploaded_file is not None:
         # }
         st.text(map_dict [prediction])
         st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
+        if map_dict [prediction] == "alba":
+            st.text_area("Market Price per 1kg:4900-5000LKR")
+        if map_dict [prediction] == "blackpeper_low":
+            st.text_area("Market Price per 1kg:2000LKR")
+        if map_dict [prediction] == "c4":
+            st.text_area("Market Price per 1kg:3800-4000LKR")
+        if map_dict [prediction] == "c4_c5":
+             st.text_area("Market Price per 1kg:3800-4000LKR")
+        if map_dict [prediction] == "extraspecial": 
+             st.text_area("Market Price per 1kg:4500-5000LKR")
+        if map_dict [prediction] == "faq": 
+             st.text_area("Market Price per 1kg:2500-3000LKR")
+        if map_dict [prediction] == "grade_1": 
+             st.text_area("Market Price per 1kg:2300-2500LKR")
+        if map_dict [prediction] == "grade_2": 
+             st.text_area("Market Price per 1kg:2000-2200LKR")
+        if map_dict [prediction] == "NoGrade": 
+             st.text_area("This Is not a Spice")
+        
